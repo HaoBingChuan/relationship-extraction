61680aae30d3c78f802d32dbe7ae9872da30b4d9
zengtao <zengtao@mogutrip.cn>
2024-06-06 17:48:09 +0800

    代码优化

--- neo4j/server.py
+++ neo4j/server.py
@@ -19,8 +19,21 @@ schema = {
         "荣誉",
     ]
 }
+schema_rel = {
+    "个人信息": [
+        "姓名",
+        "专辑",
+        "性别",
+        "祖籍",
+        "职业",
+        "毕业学校",
+        "出生日期",
+        "出生地",
+        "发布专辑"
+    ]
+}
 # device_id为gpu id，如果写-1则使用cpu
-task = Taskflow("information_extraction", schema=schema, device_id=-1)
+task = Taskflow("information_extraction", schema=schema_rel, device_id=-1)
 
 
 def parse(result):
